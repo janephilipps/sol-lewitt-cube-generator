7b973354ac607b494b19739660cf3737c4b1f360
Jane Philipps <jphilipps@pinterest.com>
2019-12-15 20:12:43 -0800

    Dave code review comments

--- src/App.js
+++ src/App.js
@@ -14,27 +14,69 @@ class Canvas extends React.Component {
 
   drawCanvas() {
     const { backgroundColor, backgroundLineColor, cubeBackgroundColor, cubeFaceLineColor, cubeTopLineColor, cubeSideLineColor } = this.props;
+
+    // Initialize the canvas.
     const canvas = this.refs.canvas;
     const ctx = canvas.getContext("2d");
-    ctx.fillRect(100, 100, 200, 200);
+
+    // maybe change "dimension" to "cubeWidth".
 
     const drawBackground = (dimension, backgroundColor, backgroundLineColor) => {
       ctx.fillStyle = backgroundColor;
-      ctx.fillRect(0, 0, 500, 500);
+      ctx.fillRect(0, 0, 500, 500); // calculate in terms of dimension
       ctx.strokeStyle = backgroundLineColor;
       for (let i = 0; i < dimension / 2 - 1; i++) {
         ctx.beginPath();
         ctx.moveTo(5 + 5 * i, 0);
         ctx.lineTo(5 + 5 * i, dimension * 2.5);
         ctx.stroke();
+
+        // consider using drawLines
+        // drawLines({
+        //   ctx: ctx,
+        //   start: {
+        //     x: 5 + 5 * i,
+        //     y: 0
+        //   },
+        //   linePoint: {
+        //     x: 5 + 5 * i,
+        //     y: dimension * 2.5,
+        //   }
+        // });
       }
     };
 
+    // begin path
+    // move to starting coordinate
+    // at least one lineTo
+    // stroke
+
+    const drawLines = (obj) => {
+      ctx.beginPath();
+      ctx.moveTo(obj.start.x, obj.start.y);
+      if (obj.linePoint) {
+        ctx.lineTo(obj.linePoint.x, obj.linePoint.y);
+      } else {
+        obj.linePoints.forEach(linePoint => {
+          ctx.lineTo(linePoint.x, linePoint.y);
+        });
+      }
+      ctx.stroke();
+    };
+
     const drawCubeOutline = dimension => {
+      // perhaps extract these to the top of drawCanvas so we don't need to re-declare them 
+      // in each sub-function.
       const half = dimension / 2;
       const oneAndAHalf = dimension * 1.5;
       const two = dimension * 2;
       const oneFourth = dimension / 4;
+      // Might be easier to turn this step into 3 sub-steps:
+      // 1. draw front
+      // 2. draw top
+      // 3. draw side
+      // I think it's fine to re-draw lines, i.e. the top front line
+
       ctx.strokeStyle = "black";
       ctx.beginPath();
       ctx.moveTo(half, half);
@@ -51,6 +93,8 @@ class Canvas extends React.Component {
       ctx.stroke();
     };
 
+    // Extract this and drawCubeOutline into a function that just takes whether
+    // we're doing fill or stroke.
     const fillCube = (dimension, color) => {
       const half = dimension / 2;
       const oneAndAHalf = dimension * 1.5;
@@ -163,6 +207,12 @@ class Canvas extends React.Component {
     };
 
     draw(200, backgroundColor, backgroundLineColor, cubeBackgroundColor, cubeFaceLineColor, cubeTopLineColor, cubeSideLineColor);
+    
+    // Perhaps use object style parameters so they are named at call site.
+    // draw({
+    //   dimension: 200,
+    //   backgroundColor: backgroundColor
+    // });
 
   }
 
@@ -242,12 +292,13 @@ class App extends React.Component {
                         {color}
                       </option>
                     );
+                  } else {
+                    return (
+                      <option value={color} key={color} style={optionStyle}>
+                        {color}
+                      </option>
+                    );
                   }
-                  return (
-                    <option value={color} key={color} style={optionStyle}>
-                      {color}
-                    </option>
-                  );
                 })}
               </select>
             </div>
